8ce2205a46d7840ae91f606043ba38d5b5a860ab
rookie reandra <septaranja@gmail.com>
2025-07-16 23:08:58 +0700

    file constant

---
+++ constants.js
@@ -0,0 +1,29 @@
+const prompts = [
+
+    ["whatsup", "apa kabar?", "selamat pagi", "selamat siang", "selamat malam", "selamat tidur", "hey bruhh", "apa kabarmu?", "hi"], // sapaan
+    ["bagaimana harimu?", "apakah suasanamu sedang baik?", "sedang stress?", 
+    "sedang depresot"], // pertanyaan 
+    ["apa yang bisa kulakukan untukmu?"],
+    [ "apa yang sedang kamu lakukan?", "apakah kamu sudah punya pacar?",]
+
+]
+
+
+const replies = [
+
+    ["whatsup too", "baik", "pagi juga", "siang juga", "malam juga", "belum mau tidur", "sok kenal", "sedang stress", "he ho"],
+    ["hari ku baik", "tidakkkk", "iyaaa", " benar sekali"]
+    ["jadi pacarku saja"]
+    ["sedang melihatmu", "sudah hahaha"]
+
+]
+
+const alternative = [
+    
+    "aku gak paham maksudmu",
+    "membosankan sekali kamu", 
+    "masa sih",
+    "sok tahu deh",
+    "aku gak tahu nih:/"
+
+]
